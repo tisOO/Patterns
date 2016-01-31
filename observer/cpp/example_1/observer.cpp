8d974763d0f3838b85f873982c572375d789bc0f
Timofey Isakov <tiso@breakingbrain.org>
2016-01-31 18:52:26 +0300

    #4 Refactor c++ pattern

--- observer/cpp/example_1/observer.cpp
+++ observer/cpp/example_1/observer.cpp
@@ -5,19 +5,22 @@
 class Observer
 {
 public:
+  virtual ~Observer() {};
   virtual void update(float temp, float humidity, float pressure) = 0;
 };
 
 class Subject
 {
 public:
+  virtual ~Subject() {};
   virtual void registerObserver(Observer* o) = 0;
 };
 
 class DisplayElement
 {
 public:
-  virtual void display() = 0;
+  virtual ~DisplayElement() {};
+  virtual void display() const = 0;
 };
 
 class WeatherData: public Subject
@@ -32,7 +35,7 @@ public:
   WeatherData() {
   }
 
-  virtual void registerObserver(Observer* o) {
+  void registerObserver(Observer* o) override {
     this->observers.push_back(o);
   }
 
@@ -73,13 +76,13 @@ public:
     weatherData->registerObserver(this);
   }
 
-  virtual void update(float temperature_, float humidity_, float pressure_) {
+  void update(float temperature_, float humidity_, float pressure_) override {
     this->temperature = temperature_;
     this->humidity = humidity_;
     this->display();
   }
 
-  virtual void display() {
+  void display() const override {
     std::cout << "Current condititions: " << this->temperature
               << "C degrees and " << this->humidity << "% humidity"
               << std::endl;
